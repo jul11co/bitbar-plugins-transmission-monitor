db64413e2a3b1dcbd2c463280013fc0aec9f231e
Manh Thiep <manhthiep@gmail.com>
2018-04-15 01:10:13 +0700

    Fix dependencies & Add free space check

--- transmission-restart-errors.10s.js
+++ transmission-restart-errors.10s.js
@@ -4,7 +4,7 @@
 // <bitbar.author>Jul11Co</bitbar.author>
 // <bitbar.author.github>Jul11Co</bitbar.author.github>
 // <bitbar.desc>Monitor Tranmission torrent downloads</bitbar.desc>
-// <bitbar.dependencies>node</bitbar.desc>
+// <bitbar.dependencies>node, npm, npm/async, npm/bytes, npm/transmission</bitbar.dependencies>
 
 // To install
 // npm install async bytes transmission 
@@ -20,6 +20,12 @@ var transmission = new Transmission({
   username: 'transmission',      // default: blank
   password: '12345678'           // default: blank
 });
+
+var cheek_free_space = false;
+var remote_path = '/INPUT/REMOTE/PATH/TO/CHECK';
+var free_space_str = '';
+
+// console.log("�");console.log('---');
 
 // Get torrent state 
 function getStatusType(type){
@@ -135,6 +141,26 @@ function startTorrentNow(id, options, callback){
   });
 }
 
+function getFreeSpace(remote_path, options, callback) {
+  if (typeof options == 'function') {
+    callback = options;
+    options = {};
+  }
+  options = options || {};
+  callback = callback || function(err) {};
+
+  // get Free Space
+  transmission.freeSpace(remote_path, function(err, result){
+    if (err){
+      return callback(err);
+    }
+
+    callback(null, result);
+  });
+}
+
+///
+
 function restartErrorTorrents(torrents, callback) {
   
   var error_torrents = torrents.filter(function(torrent) {
@@ -154,6 +180,58 @@ function restartErrorTorrents(torrents, callback) {
   });
 }
 
+function printActiveTorrents(torrents) {
+
+  var active_torrents = torrents.filter(function(torrent) {
+    return (torrent.status >= 3 && torrent.status <= 6);
+  });
+
+  active_torrents.sort(function(a,b) {
+    if (a.percentDone>b.percentDone) return -1;
+    if (a.percentDone<b.percentDone) return 1;
+    return 0;
+  });
+  active_torrents.sort(function(a,b) {
+    if (a.status>b.status) return 1;
+    if (a.status<b.status) return -1;
+    return 0;
+  });
+
+  active_torrents.forEach(function(torrent, idx) {
+    var percentage = (torrent.percentDone*100).toFixed(1);
+    var prefix = '';
+    if (idx >= 10) prefix = '--';
+    if (idx == 10 && active_torrents.length > 10) {
+      console.log('More (' + (active_torrents.length-10) + ')...');
+    } 
+    if (torrent.status == 3 && torrent.error) {
+      console.log(prefix + '⏸ ' + bytes(torrent.totalSize) + ' ' + percentage + '% ' 
+        + trimText(torrent.name,30) + ' | color=red');
+      console.log(prefix + ' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
+      console.log(prefix + "Error: "+ trimText(torrent.errorString,40) + ' | color=grey');
+    }
+    else if (torrent.status == 3) {
+      console.log(prefix + '⏸ ' + trimText(torrent.name,30) + ' | color=orange');
+      console.log(prefix + ' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
+    } 
+    else if (torrent.status == 4) {
+      console.log(prefix + '⏬ ' + trimText(torrent.name,30) + ' | color=blue');
+      console.log(prefix + ' ' + bytes(torrent.totalSize) + ', ' + percentage + '%, ' 
+        + " ▼ " + bytes(torrent.rateDownload) + '/s, ▲ ' + bytes(torrent.rateUpload) + '/s | color=grey');
+    }
+    else if (torrent.status == 6) {
+      console.log(prefix + '⏫ ' + trimText(torrent.name,30) + ' | color=green');
+      console.log(prefix + ' ' + bytes(torrent.totalSize) + ', ' + percentage + '%, ' 
+        + " ▼ " + bytes(torrent.rateDownload) + '/s, ▲ ' + bytes(torrent.rateUpload) + '/s | color=grey');
+    }
+    else if (torrent.status == 5) {
+      console.log(prefix + '⏸ ' + trimText(torrent.name,30) + ' | color=green');
+      console.log(prefix + ' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
+    }
+    console.log(prefix+'---');
+  });
+}
+
 function update(done) {
   // get all torrents
   getAllTorrents(function(err, torrents) {
@@ -162,6 +240,10 @@ function update(done) {
       return done(err);
     } else if (!torrents || torrents.length == 0) {
       console.log("🐶");console.log('---');
+      if (free_space_str) {
+        console.log(free_space_str);
+        console.log('---');
+      }
       console.log('No torrents | color=black');
       return done();
     }
@@ -179,6 +261,11 @@ function update(done) {
     var seeding = torrent_stats['SEED'] ? torrent_stats['SEED'].length : 0;
     console.log("🐶 " + downloading + '/' + seeding);console.log('---');
 
+    if (free_space_str) {
+      console.log(free_space_str);
+      console.log('---');
+    }
+
     for (var status in torrent_stats) {
       console.log(status + ': ' + torrent_stats[status].length + '');
       torrent_stats[status].forEach(function(torrent) {
@@ -190,50 +277,22 @@ function update(done) {
     }
     console.log('---');
 
-    var active_torrents = torrents.filter(function(torrent) {
-      return (torrent.status >= 3 && torrent.status <= 6);
-    });
-
-    active_torrents.sort(function(a,b) {
-      if (a.percentDone>b.percentDone) return -1;
-      if (a.percentDone<b.percentDone) return 1;
-      return 0;
-    });
-
-    active_torrents.forEach(function(torrent) {
-      var percentage = (torrent.percentDone*100).toFixed(1);
-      if (torrent.status == 3 && torrent.error) {
-        console.log('⏸ ' + bytes(torrent.totalSize) + ' ' + percentage + '% ' 
-          + trimText(torrent.name,30) + ' | color=red');
-        console.log(' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
-        console.log("Error: "+ trimText(torrent.errorString,40) + ' | color=grey');
-      }
-      else if (torrent.status == 3) {
-        console.log('⏸ ' + trimText(torrent.name,30) + ' | color=orange');
-        console.log(' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
-      } 
-      else if (torrent.status == 4) {
-        console.log('⏬ ' + trimText(torrent.name,30) + ' | color=blue');
-        console.log(' ' + bytes(torrent.totalSize) + ', ' + percentage + '%, ' 
-          + " ▼ " + bytes(torrent.rateDownload) + '/s, ▲ ' + bytes(torrent.rateUpload) + '/s | color=grey');
-      }
-      else if (torrent.status == 6) {
-        console.log('⏫ ' + trimText(torrent.name,30) + ' | color=green');
-        console.log(' ' + bytes(torrent.totalSize) + ', ' + percentage + '%, ' 
-          + " ▼ " + bytes(torrent.rateDownload) + '/s, ▲ ' + bytes(torrent.rateUpload) + '/s | color=grey');
-      }
-      else if (torrent.status == 5) {
-        console.log('⏸ ' + trimText(torrent.name,30) + ' | color=green');
-        console.log(' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
-      }
-      console.log('---');
-    });
+    printActiveTorrents(torrents);
 
     // Restart error torrents
     restartErrorTorrents(torrents, done);
   });
 }
 
-update(function(err) {});
+if (cheek_free_space) {
+  getFreeSpace(remote_path, function(err, result) {
+    if (result && result['size-bytes']) {
+      free_space_str = 'Free Space: ' + bytes(result['size-bytes']);
+    }
+    update(function(err) {});
+  });
+} else {
+  update(function(err) {});
+}
 
 
--- transmission.10s.js
+++ transmission.10s.js
@@ -4,6 +4,7 @@
 // <bitbar.author>Jul11Co</bitbar.author>
 // <bitbar.author.github>Jul11Co</bitbar.author.github>
 // <bitbar.desc>Monitor Tranmission torrent downloads</bitbar.desc>
+// <bitbar.dependencies>node, npm, npm/bytes, npm/transmission</bitbar.dependencies>
 
 // To install
 // npm install bytes transmission 
@@ -17,6 +18,10 @@ var transmission = new Transmission({
   username: 'transmission',      // default: blank
   password: '12345678'           // default: blank
 });
+
+var cheek_free_space = false;
+var remote_path = '/REMOTE/PATH/TO/CHECK';
+var free_space_str = '';
 
 // console.log("🐶");console.log('---');
 
@@ -73,6 +78,78 @@ function getAllTorrents(options, callback){
   });
 }
 
+function getFreeSpace(remote_path, options, callback) {
+  if (typeof options == 'function') {
+    callback = options;
+    options = {};
+  }
+  options = options || {};
+  callback = callback || function(err) {};
+
+  // get Free Space
+  transmission.freeSpace(remote_path, function(err, result){
+    if (err){
+      return callback(err);
+    }
+
+    callback(null, result);
+  });
+}
+
+///
+
+function printActiveTorrents(torrents) {
+
+  var active_torrents = torrents.filter(function(torrent) {
+    return (torrent.status >= 3 && torrent.status <= 6);
+  });
+
+  active_torrents.sort(function(a,b) {
+    if (a.percentDone>b.percentDone) return -1;
+    if (a.percentDone<b.percentDone) return 1;
+    return 0;
+  });
+  active_torrents.sort(function(a,b) {
+    if (a.status>b.status) return 1;
+    if (a.status<b.status) return -1;
+    return 0;
+  });
+
+  active_torrents.forEach(function(torrent, idx) {
+    var percentage = (torrent.percentDone*100).toFixed(1);
+    var prefix = '';
+    if (idx >= 10) prefix = '--';
+    if (idx == 10 && active_torrents.length > 10) {
+      console.log('More (' + (active_torrents.length-10) + ')...');
+    } 
+    if (torrent.status == 3 && torrent.error) {
+      console.log(prefix + '⏸ ' + bytes(torrent.totalSize) + ' ' + percentage + '% ' 
+        + trimText(torrent.name,30) + ' | color=red');
+      console.log(prefix + ' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
+      console.log(prefix + "Error: "+ trimText(torrent.errorString,40) + ' | color=grey');
+    }
+    else if (torrent.status == 3) {
+      console.log(prefix + '⏸ ' + trimText(torrent.name,30) + ' | color=orange');
+      console.log(prefix + ' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
+    } 
+    else if (torrent.status == 4) {
+      console.log(prefix + '⏬ ' + trimText(torrent.name,30) + ' | color=blue');
+      console.log(prefix + ' ' + bytes(torrent.totalSize) + ', ' + percentage + '%, ' 
+        + " ▼ " + bytes(torrent.rateDownload) + '/s, ▲ ' + bytes(torrent.rateUpload) + '/s | color=grey');
+    }
+    else if (torrent.status == 6) {
+      console.log(prefix + '⏫ ' + trimText(torrent.name,30) + ' | color=green');
+      console.log(prefix + ' ' + bytes(torrent.totalSize) + ', ' + percentage + '%, ' 
+        + " ▼ " + bytes(torrent.rateDownload) + '/s, ▲ ' + bytes(torrent.rateUpload) + '/s | color=grey');
+    }
+    else if (torrent.status == 5) {
+      console.log(prefix + '⏸ ' + trimText(torrent.name,30) + ' | color=green');
+      console.log(prefix + ' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
+    }
+    console.log(prefix+'---');
+  });
+}
+
 function update(done) {
   // get all torrents
   getAllTorrents(function(err, torrents) {
@@ -81,6 +158,10 @@ function update(done) {
       return done(err);
     } else if (!torrents || torrents.length == 0) {
       console.log("�");console.log('---');
+      if (free_space_str) {
+        console.log(free_space_str);
+        console.log('---');
+      }
       console.log('No torrents | color=black');
       return done();
     }
@@ -98,6 +179,11 @@ function update(done) {
     var seeding = torrent_stats['SEED'] ? torrent_stats['SEED'].length : 0;
     console.log("🐶 " + downloading + '/' + seeding);console.log('---');
 
+    if (free_space_str) {
+      console.log(free_space_str);
+      console.log('---');
+    }
+
     for (var status in torrent_stats) {
       console.log(status + ': ' + torrent_stats[status].length + '');
       torrent_stats[status].forEach(function(torrent) {
@@ -109,49 +195,20 @@ function update(done) {
     }
     console.log('---');
 
-    var active_torrents = torrents.filter(function(torrent) {
-      return (torrent.status >= 3 && torrent.status <= 6);
-    });
-
-    active_torrents.sort(function(a,b) {
-      if (a.percentDone>b.percentDone) return -1;
-      if (a.percentDone<b.percentDone) return 1;
-      return 0;
-    });
-
-    active_torrents.forEach(function(torrent) {
-      var percentage = (torrent.percentDone*100).toFixed(1);
-      if (torrent.status == 3 && torrent.error) {
-        console.log('⏸ ' + bytes(torrent.totalSize) + ' ' + percentage + '% ' 
-          + trimText(torrent.name,30) + ' | color=red');
-        console.log(' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
-        console.log("Error: "+ trimText(torrent.errorString,40) + ' | color=grey');
-      }
-      else if (torrent.status == 3) {
-        console.log('⏸ ' + trimText(torrent.name,30) + ' | color=orange');
-        console.log(' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
-      } 
-      else if (torrent.status == 4) {
-        console.log('⏬ ' + trimText(torrent.name,30) + ' | color=blue');
-        console.log(' ' + bytes(torrent.totalSize) + ', ' + percentage + '%, ' 
-          + " ▼ " + bytes(torrent.rateDownload) + '/s, ▲ ' + bytes(torrent.rateUpload) + '/s | color=grey');
-      }
-      else if (torrent.status == 6) {
-        console.log('⏫ ' + trimText(torrent.name,30) + ' | color=green');
-        console.log(' ' + bytes(torrent.totalSize) + ', ' + percentage + '%, ' 
-          + " ▼ " + bytes(torrent.rateDownload) + '/s, ▲ ' + bytes(torrent.rateUpload) + '/s | color=grey');
-      }
-      else if (torrent.status == 5) {
-        console.log('⏸ ' + trimText(torrent.name,30) + ' | color=green');
-        console.log(' ' + bytes(torrent.totalSize) + ', ' + percentage + '% | color=grey');
-      }
-      console.log('---');
-    });
+    printActiveTorrents(torrents);
 
     return done();
   });
 }
 
-update(function(err) {});
-
+if (cheek_free_space) {
+  getFreeSpace(remote_path, function(err, result) {
+    if (result && result['size-bytes']) {
+      free_space_str = 'Free Space: ' + bytes(result['size-bytes']);
+    }
+    update(function(err) {});
+  });
+} else {
+  update(function(err) {});
+}
 
